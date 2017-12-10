bd3b1c9d966842f141a40498d155ee4f972d4e71
XbaiAmy <1621810784@qq.com>
2017-12-10 12:50:41 +0800

    增加了首页点击事件的样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,16 +3,6 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
   },
   onLoad: function () {
   },

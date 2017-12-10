15322a15d949d30986d3bdcdf7e6ca91fd047dcd
XbaiAmy <1621810784@qq.com>
2017-12-10 12:52:19 +0800

    完成了自动获取系统的日期

--- pages/dairy/dairy.js
+++ pages/dairy/dairy.js
@@ -1,70 +1,61 @@
 // pages/dairy/dairy.js
+const app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  // day：星期几
+  // date：某日
+  // month：月份
   data: {
-  
+    day: "",
+    date: "",
+    month: "",
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  onShow: function () {
+      var myDate = new Date();
+      // setData设置或更新数据
+      this.setData({
+        date: myDate.getDate(),
+        month: myDate.getMonth() + 1,
+        day: week(),
+      });
+      function week(day) {
+          switch(myDate.getDay()) {
+            case 1: day="星期一"; break;
+            case 2: day="星期二"; break;
+            case 3: day="星期三"; break;
+            case 4: day="星期四"; break;
+            case 5: day="星期五"; break;
+            case 6: day="星期六"; break;
+            default: day="星期天";
+          }
+          return day;
+        }
+  },
   onLoad: function (options) {
-  
+    
   },
-
   readDetail: function() {
     wx.navigateTo({
       url: './detail/detail'
     })
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+ 
   onReady: function () {
   
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
   
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
   
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
   
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
   
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
   
   },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
   
   }

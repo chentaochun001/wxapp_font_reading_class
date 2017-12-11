0f3e523f4ef03d175f84436cd68e6ce7999f56e6
XbaiAmy <1621810784@qq.com>
2017-12-11 23:09:41 +0800

    修改了‘看看世界’的页面

--- pages/world/world.js
+++ pages/world/world.js
@@ -1,66 +1,57 @@
 // pages/world/world.js
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    activeIndex: 0,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  //点击事件
+  changeTag: function(e) {
+    var type = e.target.dataset.index;
+    console.log(type);
+    this.setData({
+      activeIndex: type
+    });
+    this.get_github_list(type);
+  },
+  //滑动事件
+  swiperTab: function(e) {
+    var type = e.detail.current;
+    this.setData({
+      activeIndex: type
+    });
+    this.get_github_list(type);
+  },
+  get_github_list: function(type) {
+    var that = this;
+    wx.request({
+      url: 'https://www.easy-mock.com/mock/5a2b5f76158e7b70032804d0/getActicleInfo/getGithubInfo',
+      method: 'GET',
+      data: {
+        type: type
+      },
+      success: function(res) {
+        if(type==0) {
+          console.log(res);
+          that.setData({
+            html_List: res.data.data.html_List
+          });
+        } else if(type==1) {
+          console.log(res);
+          that.setData({
+            css_List: res.data.data.css_List
+          });
+        } else {
+          console.log(res);
+          that.setData({
+            javascript_List: res.data.data.javascript_List
+          });
+        }
+      }
+    })
+  },
   onLoad: function (options) {
-  
+    this.get_github_list("");
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })

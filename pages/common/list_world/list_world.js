62cfc670a90577d2c05aa74aac1fe74a0881bfab
XbaiAmy <1621810784@qq.com>
2017-12-11 23:28:33 +0800

    增加了看看世界页面的列表通用模块

--- pages/common/list_world/list_world.js
+++ pages/common/list_world/list_world.js
@@ -1,16 +1,12 @@
 // pages/common/list/list.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
+  // 页面的初始数据
   data: {
   
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  // 生命周期函数--监听页面加载
   onLoad: function (options) {
   
   },

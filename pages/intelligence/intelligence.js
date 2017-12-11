02a4ece611db3fc45b910bb6f867be0dc8d3adc1
XbaiAmy <1621810784@qq.com>
2017-12-11 23:03:57 +0800

    修改了情报局页面的js

--- pages/intelligence/intelligence.js
+++ pages/intelligence/intelligence.js
@@ -1,66 +1,23 @@
 // pages/intelligence/intelligence.js
+const app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    intelligence_list: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  get_intelligence_list: function() {
+    var that = this;
+    wx.request({
+      url: 'https://www.easy-mock.com/mock/5a2b5f76158e7b70032804d0/getActicleInfo/getIntelligenceInfo',
+      method: 'GET',
+      success: function(res) {
+        console.log(res);
+        that.setData({
+          intelligence_list: res.data.data.intelligence_list
+        })
+      }
+    })
+  },
   onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    this.get_intelligence_list("");
   }
 })

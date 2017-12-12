40243f1ad0b6974a6b86981149821bd3ff6d7e09
XbaiAmy <1621810784@qq.com>
2017-12-12 23:18:39 +0800

    更新了阅读详情页面的注释

--- pages/dairy/detail/detail.js
+++ pages/dairy/detail/detail.js
@@ -3,18 +3,20 @@ Page({
     item: []
   },
   onLoad: function(params) {
+    // 检验id是否传递成功
     console.log(params.id);
     var that = this;
     wx.request({
       url: 'https://www.easy-mock.com/mock/5a2b5f76158e7b70032804d0/getActicleInfo/getActicleInfo',
       success: function(res) {
+        // 使用过滤器把id等于上一页面id的数组赋值给datas
         var datas = res.data.data.acticle_List.filter(item => {
           return item.acticle_id == params.id;
         });
         that.setData({
           item: datas
         });
-        console.log(datas)
+        console.log(datas);
       }
     })
 
